773dfe956dd869c873497e4285f411a1e882015a
Rocco-Jenson <130722422+Rocco-Jenson@users.noreply.github.com>
2024-07-15 06:45:06 -0700

    Update VehicleManager.ino

--- VehicleManager/VehicleManager.ino
+++ VehicleManager/VehicleManager.ino
@@ -1,6 +1,7 @@
 #include "vehicle_manager.h"
 
-#define ORIGIN_MILEAGE 0
+#define DEFAULT_BAUD_RATE 115200
+#define DEFAULT_ORIGIN_MILEAGE 0
 
 String input(const char* question, int status=1) noexcept {
   if (status) {
@@ -16,11 +17,11 @@ void PRINT_SUCCESS() noexcept {
   Serial.println("COMMAND SUCCESS");
 }
 
-std::vector<FluidState> FluidStateArray{FluidState::CreateFluidStates(ORIGIN_MILEAGE)};
+std::vector<FluidState> FluidStateArray{FluidState::CreateFluidStates(DEFAULT_ORIGIN_MILEAGE)};
 VehicleManager _object;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(DEFAULT_BAUD_RATE);
 
   delay(2500);
 
@@ -30,13 +31,13 @@ void setup() {
 
   if (_object.checkNotInitialized()) {
     Serial.println("Object not initialized. Filling with initial data...");
-    _object.initVehicleManager("Ford Ranger 2006 3.0L V6", ORIGIN_MILEAGE, FluidStateArray);
+    _object.initVehicleManager("Ford Ranger 2006 3.0L V6", DEFAULT_ORIGIN_MILEAGE, FluidStateArray);
     _object.saveToEEPROM();
-    Serial.println("Initial data filled and saved to EEPROM.");
+    Serial.println("Initial data filled and saved to EEPROM");
     Serial.println();
   } else {
     Serial.println("Object already initialized. Loading from EEPROM...");
-    Serial.println("Data loaded from EEPROM.");
+    Serial.println("Data loaded from EEPROM");
   }
 
   _object.checkStatus();
@@ -45,16 +46,25 @@ void setup() {
   Serial.println("Setup completed.");
 }
 void loop() {
-  String user_in = input("Input A Command -> UMS/Update Mileage Status, AN/Add Note, RN/Remove Note, SE/Save to EEPROM, CSP/Change State Parameters, DS/ Display State");
+  String user_in = input("Input A Command:\nUOMS/Update Origin Mileage Status,\nUNMS Update New Mileage Status,\nAN/Add Note,\nRN/Remove Note,\nSE/Save to EEPROM,\nCSP/Change State Parameters,\nDS/ Display State,\nCE/Clear All EEPROM");
   user_in.trim();
 
-  if ("UMS" == user_in) {
+  if ("UOMS" == user_in) {
     String value = input("Update Origin Mileage Status");
     value.trim();
-    _object.updateMileageStatus(value.toInt());
+    _object.updateOriginMileage(value.toInt());
     _object.checkStatus();
     PRINT_SUCCESS();
-  } else if ("AN" == user_in) {
+  } else if ("UNMS") {
+    String value1 = input("Select Fluid Index: 1 = EO, 2 = BF, 3 = TF, 4 = PSF, 5 = DF, 6 = C, 7 = WWF");
+    String value2 = input("Update New Mileage Status");
+    value1.trim(); value2.trim();
+    _object.updateNewMileage(value1.toInt(), value2.toInt());
+    _object.checkStatus();
+    PRINT_SUCCESS();
+  }
+  
+  else if ("AN" == user_in) {
     String value = input("Add a Note");
     value.trim();
     const char* cstr = value.c_str();
@@ -75,6 +85,8 @@ void loop() {
   } else if ("DS" == user_in) {
     _object.displayState();
     PRINT_SUCCESS();
+  } else if ("CE") {
+    VehicleManager::clearAllEEPROM();
   } else {
     Serial.print("Invalid Input");
   }
